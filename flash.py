ddd3e34a5c188153a5c222eb987da816aecbc8fe
Willem Melching <willem.melching@gmail.com>
2020-04-13 14:56:43 -0700

    Small cleanup of flash script

--- flash.py
+++ flash.py
@@ -17,29 +17,23 @@ if __name__ == "__main__":
   # Open zip file
   zip_fn = os.path.join(cur_dir, latest['version'] + '.zip')
   with zipfile.ZipFile(zip_fn) as zip_file:
-    # Check for panda in DFU mode
-    if not PandaDFU.list():
-      print("No panda in DFU mode found")
-
-      while not Panda.list():
-        print("Waiting for Panda")
-        time.sleep(0.5)
-
-      print("Putting panda in DFU mode")
-      panda = Panda()
-      panda.reset(enter_bootstub=True)
-      panda.reset(enter_bootloader=True)
-
-    # Wait for dfu
+    # Wait for panda to connect
     while not PandaDFU.list():
-      print("Waiting for DFU")
+      print("Waiting for panda in DFU mode")
+
+      if Panda.list():
+        print("Panda found. Putting in DFU Mode")
+        panda = Panda()
+        panda.reset(enter_bootstub=True)
+        panda.reset(enter_bootloader=True)
+
       time.sleep(0.5)
 
     # Flash bootstub
     bootstub_code = zip_file.open('bootstub.panda.bin').read()
     PandaDFU(None).program_bootstub(bootstub_code)
 
-    # Wait for panda
+    # Wait for panda to come back online
     while not Panda.list():
       print("Waiting for Panda")
       time.sleep(0.5)
