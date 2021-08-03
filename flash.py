62b560d904742424f64a38ce98513143ee9d2946
Igor Biletksyy <bs@privacy.im>
2021-08-03 15:53:43 -0700

    First release with red panda support

--- flash.py
+++ flash.py
@@ -3,8 +3,13 @@ import time
 import os
 import json
 import zipfile
-from panda import Panda, PandaDFU
+from panda import Panda, PandaDFU, MCU_TYPE_H7
 
+BOOTSTUB_H7_FN = "bootstub.panda_h7.bin"
+BOOTSTUB_FX_FN = "bootstub.panda.bin"
+FW_H7_FN = "panda_h7.bin"
+FW_FX_FN = "panda.bin"
+is_mcu_h7 = False
 
 if __name__ == "__main__":
   cur_dir = os.path.dirname(os.path.realpath(__file__))
@@ -30,8 +35,12 @@ if __name__ == "__main__":
       time.sleep(0.5)
 
     # Flash bootstub
-    bootstub_code = zip_file.open('bootstub.panda.bin').read()
-    PandaDFU(None).program_bootstub(bootstub_code)
+    panda_dfu = PandaDFU(None)
+    is_mcu_h7 = panda_dfu._mcu_type == MCU_TYPE_H7
+    fn = BOOTSTUB_H7_FN if is_mcu_h7 else BOOTSTUB_FX_FN
+    print(f"Detected MCU type {panda_dfu._mcu_type}, flashing {fn}")
+    bootstub_code = zip_file.open(fn).read()
+    panda_dfu.program_bootstub(bootstub_code)
 
     # Wait for panda to come back online
     while not Panda.list():
@@ -39,5 +48,7 @@ if __name__ == "__main__":
       time.sleep(0.5)
 
     # Flash firmware
-    firmware_code = zip_file.open('panda.bin').read()
+    fn = FW_H7_FN if is_mcu_h7 else FW_FX_FN
+    print(f"Flashing {fn}")
+    firmware_code = zip_file.open(fn).read()
     Panda().flash(code=firmware_code)
